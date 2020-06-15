6808c95a43d34827c0d7f4a3c4eaf7e781ac49d0
Rafaela Guimarães <rguimaraes@inoa.com.br>
2020-06-14 22:17:34 -0300

    chore: add product filtering

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
+const promotions = ["SINGLE LOOK", "DOUBLE LOOK", "TRIPLE LOOK", "FULL LOOK"];
+
+const filterProducts = (ids, productsList) =>
+  ids.map((id) => productsList.find((product) => id == product.id));
+
+const getProducts = (productsList) =>
+  productsList.map((product) => ({
+    name: product["name"],
+    category: product["category"],
+  }));
 
 function getShoppingCart(ids, productsList) {
-	return {};
+  data = {};
+  products = filterProducts(ids, productsList);
+  data["products"] = getProducts(products);
+  return data;
 }
 
 module.exports = { getShoppingCart };
